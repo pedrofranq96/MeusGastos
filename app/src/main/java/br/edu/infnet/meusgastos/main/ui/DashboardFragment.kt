7add46ab15be823472e8f668e5fb85b1c53e73d2
Guilherme <prfsantosgui546@gmail.com>
2022-11-29 16:17:16 -0300

    Recycler View Despesas

--- app/src/main/java/br/edu/infnet/meusgastos/main/ui/DashboardFragment.kt
+++ app/src/main/java/br/edu/infnet/meusgastos/main/ui/DashboardFragment.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.infnet.meusgastos.R
 import br.edu.infnet.meusgastos.databinding.FragmentDashboardBinding
+import br.edu.infnet.meusgastos.main.ui.adapters.DespesaComIdAdapter
+import br.edu.infnet.meusgastos.main.ui.adapters.DespesaComIdListener
+import br.edu.infnet.meusgastos.models.DespesaComId
 import br.edu.infnet.meusgastos.utils.nav
 
 class DashboardFragment : Fragment(){
+
+    val viewModel: MainViewModel by activityViewModels()
 
     private var _binding: FragmentDashboardBinding? = null
     // This property is only valid between onCreateView and
@@ -31,6 +38,40 @@ class DashboardFragment : Fragment(){
     private fun setup() {
         setupViews()
         setupClickListeners()
+        setupRecyclerView()
+        setupObservers()
+    }
+
+    val adapter = DespesaComIdAdapter(
+       object : DespesaComIdListener{
+           override fun onEditClick(despesa: DespesaComId) {
+               viewModel.setSelectedDespesaComId(despesa)
+           }
+
+           override fun onDeleteClick(despesa: DespesaComId) {
+               viewModel.deleteDespesa(despesa.id)
+           }
+       }
+    )
+
+    private fun setupRecyclerView() {
+        binding.rvDashboardDespesas.adapter = adapter
+        binding.rvDashboardDespesas.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+    }
+
+    private fun setupObservers(){
+        viewModel.despesasComId.observe(viewLifecycleOwner){
+            atualizaRecyclerView(it)
+        }
+    }
+
+    fun atualizaRecyclerView(lista: List<DespesaComId>?) {
+            adapter.submitList(lista)
+            binding.rvDashboardDespesas.adapter = adapter
     }
 
     private fun setupViews() {
---
+++ app/src/main/java/br/edu/infnet/meusgastos/main/ui/adapters/DespesaComIdAdapter.kt
@@ -0,0 +1,77 @@
+package br.edu.infnet.meusgastos.main.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.edu.infnet.meusgastos.databinding.DespesaListItemBinding
+import br.edu.infnet.meusgastos.databinding.FragmentDashboardBinding
+import br.edu.infnet.meusgastos.models.DespesaComId
+
+class DespesaComIdAdapter(val listener: DespesaComIdListener) :
+    ListAdapter<
+            DespesaComId,
+            DespesaComIdAdapter.ViewHolder
+            >(DespesaComIdDiffCallback()) {
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val item = getItem(position)
+            holder.bind(item, position)
+        }
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ViewHolder {
+            return ViewHolder.from(parent, listener)
+        }
+
+        class ViewHolder private constructor(
+            val binding: DespesaListItemBinding,
+            val listener: DespesaComIdListener
+        ) : RecyclerView.ViewHolder(binding.root) {
+            fun bind(item: DespesaComId, position: Int) {
+                binding.apply {
+                    tvRvNome.text = item.nome
+                    tvRvValor.text = item.valor.toString()
+                    tvRvData.text = item.data
+
+
+//                    ivEdit.setOnClickListener {
+//                        listener.onEditClick(item)
+//                    }
+//                    ivDelete.setOnClickListener {
+//                        listener.onDeleteClick(item)
+//                    }
+
+                }
+            }
+
+            companion object {
+                fun from(parent: ViewGroup, listener: DespesaComIdListener): ViewHolder {
+                    val layoutInflater = LayoutInflater.from(parent.context)
+                    val binding = DespesaListItemBinding.inflate(
+                        layoutInflater, parent, false
+                    )
+                    return ViewHolder(binding, listener)
+                }
+            }
+        }
+    
+}
+class DespesaComIdDiffCallback : DiffUtil.ItemCallback<DespesaComId>() {
+
+    override fun areItemsTheSame(oldItem: DespesaComId, newItem: DespesaComId): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: DespesaComId, newItem: DespesaComId): Boolean {
+        return oldItem == newItem
+    }
+}
+
+    interface DespesaComIdListener {
+        fun onEditClick(despesa: DespesaComId)
+        fun onDeleteClick(despesa: DespesaComId)
+    }    
