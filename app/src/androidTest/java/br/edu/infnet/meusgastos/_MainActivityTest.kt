d163e767d1b30e80e5d8f00c7bdc0e9190bbe3fd
pedrofranq96 <pedrofranq96@gmail.com>
2022-12-14 19:37:04 -0300

    Testes instrumentados - Login e cadastro.

---
+++ app/src/androidTest/java/br/edu/infnet/meusgastos/_MainActivityTest.kt
@@ -0,0 +1,55 @@
+package br.edu.infnet.meusgastos
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import br.edu.infnet.meusgastos.main.ui.MainActivity
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class _MainActivityTest {
+
+    @JvmField
+    @Rule
+    val rule = ActivityScenarioRule<MainActivity>(MainActivity::class.java)
+
+
+    @Test
+    fun rightPassword_should_startActivity(){
+        onView(withId(R.id.btn_pular)).perform(click())
+
+        onView(withId(R.id.input_email)).perform(
+            typeText("teste@teste.com"),
+            ViewActions.closeSoftKeyboard()
+        )
+
+        onView(withId(R.id.input_password)).perform(
+            typeText("753951"),
+            ViewActions.closeSoftKeyboard()
+        )
+    }
+
+    @Test
+    fun inputRightRegisterData() {
+
+
+        onView(withId(R.id.btn_pular)).perform(click())
+
+        onView(withId(R.id.btn_sign_on)).perform(click())
+
+        onView(withId(R.id.input_email2)).perform(typeText("teste@instumented.com"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.input_password2)).perform(typeText("a11111"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.input_confirm_password2)).perform(typeText("a11111"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.btn_cadastrar)).perform(click())
+    }
+
+}
